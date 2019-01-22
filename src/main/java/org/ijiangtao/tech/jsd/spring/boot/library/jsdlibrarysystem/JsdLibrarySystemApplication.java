ea8abf902782db26de7a37ce2e5dc93b282d05d2
jiangtao.li <jiangtao.li@baozun.com>
2019-01-22 22:31:15 +0800

    init

---
+++ src/main/java/org/ijiangtao/tech/jsd/spring/boot/library/jsdlibrarysystem/JsdLibrarySystemApplication.java
@@ -0,0 +1,14 @@
+package org.ijiangtao.tech.jsd.spring.boot.library.jsdlibrarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsdLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsdLibrarySystemApplication.class, args);
+	}
+
+}
+
